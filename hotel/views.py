46198c79fe1f10a7b777c9764c725190837fb96a
AmirabbasAfrasiyabi <amirabbasafrasiyabi1998@gmail.com>
2026-07-12 22:55:25 +0330

    updated

--- hotel/views.py
+++ hotel/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hotel_view(request):
-    return render(request,'hotel/hotel.html')
+    return render(request,'hotel/hotels.html')
 
