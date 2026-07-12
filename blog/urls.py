4b70670dbac88adb6c30b0ef9d6c5b668d160201
AmirabbasAfrasiyabi <amirabbasafrasiyabi1998@gmail.com>
2026-07-12 12:35:21 +0330

    updated base.html templated

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from blog import views
 from blog.views import blog_view , blog_single
 app_name = ('blog')
 urlpatterns = [
-    path('', blog_view, name='blog'),
+    path('', blog_view, name='index'),
     path('single', blog_single, name='single'),
 ]
