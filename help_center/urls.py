7e1d842bf241f70e2c0d8ff34543a57daaa454ac
AmirabbasAfrasiyabi <amirabbasafrasiyabi1998@gmail.com>
2026-07-18 11:05:56 +0330

    updated

--- help_center/urls.py
+++ help_center/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from help_center.views import *
 app_name = ('help_center')
 urlpatterns = [
-    path('', help_center_FAQ, name='FAQ'),
+    path('faq', help_center_FAQ, name='FAQ'),
+    path('Terms_and_Conditions', help_center_Terms_and_Conditions, name='Terms_and_Conditions'),
+
 ]
--- help_center/views.py
+++ help_center/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def help_center_FAQ(request):
-    return render(request,'help_center/help_center_FAQ.html')
+    return render(request,'help_center/FAQ.html')
+
+def help_center_Terms_and_Conditions(request):
+    return render(request,'help_center/Terms_and_Conditions.html')
