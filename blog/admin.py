89126ad0f514023e155392c5ee9b4265fd50973c
AmirabbasAfrasiyabi <amirabbasafrasiyabi1998@gmail.com>
2026-07-25 12:02:19 +0330

    created models for blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from blog.models import *
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'published_date'
+    empty_value_display = '-empty-'
+    list_display = ('id', 'title', 'author', 'counted_views', 'status', 'created_date', 'updated_date')
+    list_filter = ('id','status','category','author')
+    search_fields = ('title','id','status','author')
+    list_per_page = 20
+
+    fieldsets = (
+        ('Content Information', {
+            'fields': ('title', 'content', 'image')
+        }),
+        ('Author & Category', {
+            'fields': ('author', 'category')
+        }),
+        ('Publication Settings', {
+            'fields': ('status', 'published_date')
+        }),
+        ('Statistics', {
+            'fields': ('counted_views',)
+        }),
+    )
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='blog' , default='blog/default.jpg')
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    category = models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True , blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'
+
+    def __str__(self):
+        return "{} - {}".format(self.id, self.title)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
