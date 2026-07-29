ad44c929970ba924d146a06f33db3eddcf2c7cf6
AmirabbasAfrasiyabi <amirabbasafrasiyabi1998@gmail.com>
2026-07-29 16:41:34 +0330

    solve counted view problem

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,31 @@
-from django.http import HttpResponse
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.db.models import F
 
 from blog.models import Post
 
-
-# Create your views here.
 def blog_view(request):
-    posts = Post.objects.filter(status=1)
-    context = {'posts':posts}
-    return render(request, 'blog/blog-home.html' , context)
-
-def blog_single(request,pid):
-    # posts = Post.objects.get(id=pid)
-    posts = get_object_or_404(Post,pk=pid , status=1)
+    posts = Post.objects.filter(
+        status=1,
+        published_date__lte=timezone.now()
+    )
     context = {'posts': posts}
-    return render(request, 'blog/blog-single.html' , context)
+    return render(request, 'blog/blog-home.html', context)
+
+
+def blog_single(request, pid):
+    post = get_object_or_404(
+        Post,
+        pk=pid,
+        status=1,
+        published_date__lte=timezone.now()
+    )
+
+    Post.objects.filter(pk=post.pk).update(
+        counted_views=F('counted_views') + 1
+    )
+
+    post.refresh_from_db()
+
+    context = {'posts': post}
+    return render(request, 'blog/blog-single.html', context)
