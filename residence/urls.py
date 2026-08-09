018328c4641cdc2aee7c2e4f74b6e95666353cef
AmirabbasAfrasiyabi <amirabbasafrasiyabi1998@gmail.com>
2026-08-09 11:15:06 +0330

    repaire and update file for residence app . another commit we imporove urls informations

--- residence/urls.py
+++ residence/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from transport.views import *
-app_name = 'transport'
+from residence.views import *
+app_name = 'residence'
 urlpatterns = [
+    path('hotel' , hotel_view , name="hotel"),
+    path ('village' , village_view ,name="village"),
 ]
--- residence/views.py
+++ residence/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from pip._internal import req
+
 
 # Create your views here.
+
+def hotel_view(request):
+    return render(request,'residence/hotels.html')
+
+def village_view(request):
+    return render(request,'residence/village.html')
