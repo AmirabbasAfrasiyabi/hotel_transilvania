7d0ca03660585133830b90da44ecb11bea330ab9
AmirabbasAfrasiyabi <amirabbasafrasiyabi1998@gmail.com>
2026-08-09 10:06:26 +0330

    create residence and transport app and delete hotel app

--- hotel/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from hotel.views import *
-app_name = 'hotel'
-urlpatterns = [
-    path('', hotel_view, name='index'),
-]
--- hotel/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def hotel_view(request):
-    return render(request,'hotel/hotels.html')
-
