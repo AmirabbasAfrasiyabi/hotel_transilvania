0a7c65e23544dc0c7ed7a8ffad79398b3fd28bb7
AmirabbasAfrasiyabi <amirabbasafrasiyabi1998@gmail.com>
2026-08-23 08:10:30 +0330

    create inclusion tags simple tags filter

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,25 @@
 from django import template
 
+from blog.models import Post
+
 register = template.Library()
+
+
+@register.simple_tag(name='total_posts')
+def total_posts():
+    post = Post.objects.filter(status=1).count()
+    return {'total_posts': post}
+
+@register.simple_tag(name='posts')
+def total_posts():
+    post = Post.objects.filter(status=1).count()
+    return {'total_posts': post}
+
+@register.filter()
+def snippets(value,args=20):
+    return value[:args]
+
+@register.inclusion_tag('PopularPosts.html')
+def popular_posts():
+    posts = Post.objects.filter(status=1).order_by('published_date')[:20]
+    return {'posts': posts}
