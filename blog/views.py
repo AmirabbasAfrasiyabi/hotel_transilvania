c43fbbdc1c89c09ac4f8e129f1f53b9c3f9d1745
AmirabbasAfrasiyabi <amirabbasafrasiyabi1998@gmail.com>
2026-08-23 15:11:12 +0330

    work with pagination files

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,43 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.db.models import F, Q
+from django.core.paginator import Paginator
 
 from blog.models import Post
 
-def blog_view(request,**kwargs):
+
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(
         status=1,
         published_date__lte=timezone.now()
     )
+
+    # Category filter
     if kwargs.get('cat_name'):
         posts = posts.filter(
             category__name__iexact=kwargs['cat_name']
         )
 
+    # Author filter
     if kwargs.get('author_username'):
         posts = posts.filter(
             author__username=kwargs['author_username']
         )
 
-    context = {'posts': posts}
-    return render(request, 'blog/blog-home.html', context)
+    # Pagination
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(
+        request,
+        'blog/blog-home.html',
+        context
+    )
 
 
 def blog_single(request, pid):
@@ -31,9 +48,11 @@ def blog_single(request, pid):
         published_date__lte=timezone.now()
     )
 
+    # Increase views
     Post.objects.filter(pk=post.pk).update(
         counted_views=F('counted_views') + 1
     )
+
     post.refresh_from_db()
 
     base_qs = Post.objects.filter(
@@ -48,22 +67,42 @@ def blog_single(request, pid):
     prev_post = base_qs.filter(
         published_date__lt=post.published_date
     ).order_by('-published_date').first()
+
     context = {
         'posts': post,
         'next_post': next_post,
         'prev_post': prev_post,
     }
-    return render(request, 'blog/blog-single.html', context)
+
+    return render(
+        request,
+        'blog/blog-single.html',
+        context
+    )
+
 
 def blog_category(request, cat_name):
     posts = Post.objects.filter(
         status=1,
-        category__name=cat_name,
+        category__name__iexact=cat_name,
         published_date__lte=timezone.now()
     )
 
-    context = {'posts': posts}
-    return render(request, 'blog/blog-home.html', context)
+    # Pagination
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(
+        request,
+        'blog/blog-home.html',
+        context
+    )
+
 
 def blog_search(request):
     posts = Post.objects.filter(
@@ -79,5 +118,18 @@ def blog_search(request):
             Q(content__icontains=search)
         )
 
-    context = {'posts': posts}
-    return render(request, 'blog/blog-home.html', context)
+    # Pagination
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+
+    context = {
+        'posts': posts,
+        'search': search,
+    }
+
+    return render(
+        request,
+        'blog/blog-home.html',
+        context
+    )
